facc7f1dc4ad5434927cc1ce78b9c7d3df5d8389
Gabriel Miranda <gabrielmirandabarros98@gmail.com>
2023-04-13 21:27:36 -0300

    Objeto Pessoa

---
+++ src/main/java/com/primeirotrampodev/objetopessoa/ObjetopessoaApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.objetopessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetopessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetopessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/objetopessoa/ObjetopessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.objetopessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetopessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
